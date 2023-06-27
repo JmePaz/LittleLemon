6d9209de78e869cb5b708ee5899819c2d25704c3
James Michael E. Paz <105730089+JmePaz@users.noreply.github.com>
2023-06-27 17:09:21 +0800

    Adding dynamically UI

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.littlelemon
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var topLayout:LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //code
+        topLayout = findViewById(R.id.topLayout)
+        topLayout.setBackgroundColor(Color.parseColor("#314E31"))
+        var textView:TextView = TextView(this)
+        textView.text = resources.getText(R.string.little_lemon)
+        textView.setTextColor(Color.parseColor("#EEEEEE"))
+        topLayout.addView(textView)
     }
 }
